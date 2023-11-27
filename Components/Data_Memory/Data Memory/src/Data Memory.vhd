d4783833101a03abefb2ec4340af458f221d3e97
Brice Zimmerman <bricetoddzimmerman@gmail.com>
2023-11-27 14:49:08 -0500

    added some helpful stuff

--- Components/Data_Memory/Data Memory/src/Data Memory.vhd
+++ Components/Data_Memory/Data Memory/src/Data Memory.vhd
@@ -18,30 +18,30 @@ use IEEE.NUMERIC_STD.ALL;
 
 entity Data_Memory is
     port (
-        clk: in std_logic;
-        mem_access_addr: in std_logic_vector(15 downto 0);
-        mem_write_data: in std_logic_vector(15 downto 0);
-        mem_write_en, mem_read: in std_logic;
-        mem_read_data: out std_logic_vector(15 downto 0)
+        clk: in std_logic;								   -- processor clock
+        mem_access_addr: in std_logic_vector(15 downto 0); -- memory address to access
+        mem_write_data: in std_logic_vector(15 downto 0);  -- write data to be written to memory address
+        mem_write_en, mem_read: in std_logic;			   -- write enable and read enable
+        mem_read_data: out std_logic_vector(15 downto 0)   -- read data from specified memory address
     );
 end entity Data_Memory;
 
 architecture Behavioral of Data_Memory is
-    signal ram_addr: std_logic_vector(7 downto 0);
-    type DataMemType is array (0 to 255) of std_logic_vector(15 downto 0);
-    signal RAM: DataMemType := (others => (others => '0'));
+    signal ram_addr: std_logic_vector(7 downto 0);						   -- memory addresses used for indexing the ram array
+    type DataMemType is array (0 to 255) of std_logic_vector(15 downto 0); -- defines the array type for the ram, each are 16-bits
+    signal RAM: DataMemType := (others => (others => '0'));				   -- actual ram array init with 0's
 begin
-    ram_addr <= mem_access_addr(8 downto 1);
+    ram_addr <= mem_access_addr(8 downto 1);						   -- populate the ram addresses from memory
 
     process(clk)
     begin
-        if rising_edge(clk) then
-            if mem_write_en = '1' then
-                RAM(to_integer(unsigned(ram_addr))) <= mem_write_data;
+        if rising_edge(clk) then									   -- when rising edge of clock
+            if mem_write_en = '1' then								   -- and the write mem is enabled
+                RAM(to_integer(unsigned(ram_addr))) <= mem_write_data; -- then we write the data to the ram at the specified ram address (ram_addr)
             end if;
         end if;
     end process;
 
-    mem_read_data <= RAM(to_integer(unsigned(ram_addr))) when mem_read = '1' else (others => '0');
+    mem_read_data <= RAM(to_integer(unsigned(ram_addr))) when mem_read = '1' else (others => '0'); -- else read the data output based on the ram content. If the read is not enabled then it outputs 0's
 end architecture Behavioral;
 

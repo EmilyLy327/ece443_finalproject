1875e4a55936a6f46010a56e6babc387ba36e4ce
Brice Zimmerman <bricetoddzimmerman@gmail.com>
2023-12-01 09:23:15 -0500

    fixed compile error with decode/execute idk if it works correctly tho lol

--- Simple_Processor/Simple Processor/src/Instruction Decode Execute.vhd
+++ Simple_Processor/Simple Processor/src/Instruction Decode Execute.vhd
@@ -30,16 +30,15 @@ end entity Processor;
 architecture Structural of Processor is
 
     signal opcode : std_logic_vector(2 downto 0);
-    signal internal_i_type, internal_r_type : std_logic;
-    signal i_type_reg, r_type_reg : std_logic; -- Intermediate signals to hold instruction decode fields   
-
-    signal r_a, r_b, r_c, r_d : std_logic_vector(3 downto 0);
+    signal internal_i_type, internal_r_type : std_logic; -- store bits from the instruction signal that represent the type of instruction
+    signal i_type_reg, r_type_reg : std_logic; -- intermediate signals to hold instruction decode fields   
+	signal source_register, destination_register, source_operand, destination_operand : std_logic_vector(3 downto 0);
     
-    -- Updated immediate value length 
+    -- updated immediate value length 
     signal immediate_value : std_logic_vector(15 downto 0);
 	
 	
- -- Components
+ -- components
     component RegisterFile
         port (
             clk : in std_logic;
@@ -66,36 +65,37 @@ architecture Structural of Processor is
         );
     end component DataMemory;
 
-    -- Instantiate components
     signal regfile_read_data1, regfile_read_data2 : std_logic_vector(15 downto 0);
     signal alu_result : std_logic_vector(15 downto 0);
-    signal data_memory_read_data, data_memory_write_data : std_logic_vector(15 downto 0);
+    signal data_memory_read_data, data_memory_write_data : std_logic_vector(15 downto 0);	
+	signal instruction_type : std_logic_vector(1 downto 0);
 	
 begin
 
-    -- Extract opcode and instruction decode fields
+    -- extract opcode and instruction decode fields
     opcode <= instruction(15 downto 13);  
     
     internal_i_type <= instruction(11);  
     internal_r_type <= instruction(12);
 
-   -- Instruction decoding logic
+   -- instruction decoding logic
    process(clk) 
    begin
      if rising_edge(clk) then  
        i_type_reg <= internal_i_type;
-       r_type_reg <= internal_r_type; 
+       r_type_reg <= internal_r_type;
+	   instruction_type <= i_type_reg & r_type_reg;
      end if;
    end process;
 	
-      -- Extract fields based on instruction type
-    r_a <= instruction(8 downto 5);
-    r_b <= instruction(4 downto 1);
-    r_c <= instruction(12 downto 9);
-    r_d <= instruction(8 downto 5);
+      -- extract fields based on instruction type
+    source_register <= instruction(8 downto 5);
+    destination_register <= instruction(4 downto 1);
+    source_operand <= instruction(12 downto 9);
+    destination_operand <= instruction(8 downto 5);
     immediate_value <= instruction(15 downto 0);   -- this seems wrong but won't compile with the last 8 bits
 
-    -- Instantiate and connect components
+    -- instantiate and connect components
     --RegisterFile_Inst : RegisterFile
     --    port map (
     --        clk => clk,
@@ -126,17 +126,17 @@ begin
 
 
 
-	-- Initialization or assignment of values
+	-- initialization or assignment of values
 	i_type_reg <= '0'; -- Assign initial value
 	r_type_reg <= '1'; -- Assign initial value
 	
-	-- Main Process
-    -- Connect components based on instruction type
+	-- main Process
+    -- connect components based on instruction type
     process (clk)
     begin
         if rising_edge(clk) then
 			-- Case statement with intermediate signals
-             case i_type_reg & r_type_reg is
+             case instruction_type is
 			    when "00" => -- R-Type
                     result <= alu_result;
                 when "01" => -- I-Type, Load Immediate
